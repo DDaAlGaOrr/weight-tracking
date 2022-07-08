e009ed29f341cbdf8fb48a5fff61bb40d0ef3947
Daniel Gamez Orozco <daalgaor.17@gmail.com>
2022-07-08 15:33:53 -0700

    add error messages

--- src/dto/create-user.dto.ts
+++ src/dto/create-user.dto.ts
@@ -3,46 +3,52 @@ import {
     IsNotEmpty,
     IsNumber,
     IsString,
-    MaxLength,
+    Min,
     MinLength,
 } from '@nestjs/class-validator'
 
 export class ValidateCreateUser {
-    @IsNotEmpty()
     @IsString()
+    @IsNotEmpty({ message: 'Usuario requerido ' })
     username: string
 
     @IsEmail()
-    @IsNotEmpty()
+    @IsNotEmpty({ message: 'Email requerido' })
     email: string
 
-    @IsNotEmpty()
     @IsString()
-    @MinLength(8)
+    @MinLength(8, {
+        message: 'La contraseña debe de tener minimo 8 caracteres',
+    })
+    @IsNotEmpty({ message: 'Contraseña requerida' })
     password: string
 
-    @IsNotEmpty()
+    @IsNotEmpty({ message: 'Nombre requerido' })
     @IsString()
     firstname: string
 
     @IsString()
-    @IsNotEmpty()
+    @IsNotEmpty({ message: 'Apellidos requeridos' })
     lastname: string
 
     @IsNumber()
-    @IsNotEmpty()
+    @IsNotEmpty({ message: 'Edad requerida' })
+    @Min(10, { message: 'la edad minima es de 10 años' })
     age: number
 
     @IsNumber()
     @IsNotEmpty()
+    @Min(0)
     actualWeight: number
 
     @IsNumber()
-    @IsNotEmpty()
+    @IsNotEmpty({ message: 'Meta requerida' })
+    @Min(0, { message: 'Entrada no valido' })
     targetWeight: number
 
     @IsNumber()
-    @IsNotEmpty()
+    @IsNotEmpty({ message: 'Altura requerida' })
+    @Min(0, { message: 'Entrada no valido' })
     height: number
 
     @IsString()
