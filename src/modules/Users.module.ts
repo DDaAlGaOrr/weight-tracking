f270555023b565bc29f8f4172d686dbac902971a
Albert Sanchez <albert.san.iza@gmail.com>
2022-07-19 14:57:33 -0700

    Update Users.module.ts

--- src/modules/Users.module.ts
+++ src/modules/Users.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 
-import { UsersController } from './../controllers/Users.controller'
-import { User, userSchema } from './../schemas/User.schema'
-import { UsersService } from './../services/Users.service'
+import { UsersController } from '../controllers/Users.controller'
+import { User, userSchema } from '../schemas/User.schema'
+import { UsersService } from '../services/Users.service'
 
 @Module({
     controllers: [UsersController],
